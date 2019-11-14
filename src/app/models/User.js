aa3364ad9326f7325519cbc6f14055bb3a286add
Alberto Zaranza <albertozaranza@outlook.com>
2019-11-14 19:49:04 -0300

    create user controller

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -6,7 +6,7 @@ class User extends Model {
       {
         name: Sequelize.STRING,
         email: Sequelize.STRING,
-        password_hard: Sequelize.STRING,
+        password_hash: Sequelize.STRING,
         teacher: Sequelize.BOOLEAN,
         // password: Sequelize.VIRTUAL,
       },
