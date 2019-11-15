b174cc6de90fabf7b21d3645b34afb4bd36ff6f9
Alberto Zaranza <albertozaranza@outlook.com>
2019-11-14 22:06:05 -0300

    add auth middleware

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -12,6 +12,30 @@ class UserController {
 
     return res.json({ id, name, email, teacher });
   }
+
+  async update(req, res) {
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({
+        where: { email },
+      });
+
+      if (userExists) {
+        return res.status(400).json({ error: 'User already exists' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    const { id, name, teacher } = await user.update(req.body);
+
+    return res.json({ id, name, email, teacher });
+  }
 }
 
 export default new UserController();
